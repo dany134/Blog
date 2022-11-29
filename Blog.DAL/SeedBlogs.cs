ccfcf3b8014d4a3dd60c94cb07abcb545629f475
Daniel Drmic <danieldrmic@gmail.com>
2022-11-29 20:36:18 +0100

    Add seed data

--- Blog.DAL/SeedBlogs.cs
+++ Blog.DAL/SeedBlogs.cs
@@ -30,8 +30,60 @@ namespace Blog.DAL
                         UpdatedAt = DateTime.Now
                     };
                     context.BlogPosts.Add(blog);
-                    await context.SaveChangesAsync();
+                    var blog2 = new BlogPost
+                    {
+                        Slug = "test-slug-1",
+                        Body = "Test Slug 1",
+                        Title = "This is a test title ",
+                        Description = "Description.",
+                        Tags = "A1, A2, A3",
+                        CreatedAt = DateTime.Now,
+                        UpdatedAt = DateTime.Now
+                    };
+                    context.BlogPosts.Add(blog2);
+                    var blog3 = new BlogPost
+                    {
+                        Slug = "test-slug-2",
+                        Body = "This is a second test body for seeding.",
+                        Title = "Test Slug 2",
+                        Description = "Second description.",
+                        Tags = "B1, B2, B3",
+                        CreatedAt = DateTime.Now,
+                        UpdatedAt = DateTime.Now
+                    };
+                    context.BlogPosts.Add(blog3);
+
                 }
+                if (!context.Comments.Any()) 
+                {
+                    var comment = new Comment
+                    {
+                        Body = "Comment on first post",
+                        Slug = "augmented-reality-ios-application",
+                        CreatedAt = DateTime.Now,
+                        UpdatedAt = DateTime.Now
+                    };
+                    var comment2 = new Comment
+                    {
+                        Body = "Second comment on first post",
+                        Slug = "augmented-reality-ios-application",
+                        CreatedAt = DateTime.Now,
+                        UpdatedAt = DateTime.Now
+                    };
+                    context.Comments.Add(comment2);
+
+                    var comment3 = new Comment
+                    {
+                        Body = "First comment on second post",
+                        Slug = "test-slug-1",
+                        CreatedAt = DateTime.Now,
+                        UpdatedAt = DateTime.Now
+                    };
+                    context.Comments.Add(comment3);
+
+                }
+                await context.SaveChangesAsync();
+
             }
             catch (Exception ex)
             {
