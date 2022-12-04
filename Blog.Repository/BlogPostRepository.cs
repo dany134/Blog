9eb3aff09b408a722bf0870c61e512266f83bfbc
Daniel Drmic <danieldrmic@gmail.com>
2022-12-04 15:30:51 +0100

    Distinct tags

--- Blog.Repository/BlogPostRepository.cs
+++ Blog.Repository/BlogPostRepository.cs
@@ -68,7 +68,7 @@ namespace Blog.Repository
 
         public async Task<List<string>> GetTagsAsync()
         {
-            var tags = await _context.BlogPosts.Select(x => x.Tags).ToListAsync();
+            var tags = await _context.BlogPosts.Select(x => x.Tags).Distinct().ToListAsync();
             var list = new List<string>();
             foreach (var item in tags)
             {
